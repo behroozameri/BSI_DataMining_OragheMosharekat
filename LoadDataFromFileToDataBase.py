be02dd5dbb0ddf55f9d80fc8f63ff02f584f5bde
Behrooz Ameri <behrooz.ameri@gmail.com>
2023-01-10 14:00:25 +0330

    Update LoadDataFromFileToDataBase.py

--- LoadDataFromFileToDataBase.py
+++ LoadDataFromFileToDataBase.py
@@ -1,5 +1,7 @@
 import openpyxl
 import mysql.connector
+import os
+from os.path import dirname
 #-----------------------------------------------------------------------------------------------
 def open_Connection():
     return mysql.connector.connect(user='root', 
@@ -8,9 +10,6 @@ def open_Connection():
                               port=3307,
                               database='Oraghe_Mosharekat',
                               auth_plugin='mysql_native_password')
-#-----------------------------------------------------------------------------------------------
-def close_Connection(cnx):
-    cnx.close()
 #-----------------------------------------------------------------------------------------------
 def add_Info(cnx, tableName, bran, amount):
     cursor = cnx.cursor()
@@ -22,29 +21,30 @@ def add_Info(cnx, tableName, bran, amount):
 #-----------------------------------------------------------------------------------------------
 def loadDataFromFile():
     cnx = open_Connection()
-    wookbook = openpyxl.load_workbook("input.xlsx")
+    dir_path = os.path.dirname(os.path.realpath(__file__))
+    dir_path = dirname(dir_path) + '\\files\input.xlsx'
+    wookbook = openpyxl.load_workbook(dir_path)
     sheetnames = wookbook.sheetnames
     count = 0
     worksheet = wookbook[sheetnames[0]]
-    for i in range(1, worksheet.max_row):
+    for i in range(2, worksheet.max_row):
         add_Info(cnx, 'Report_202', worksheet.cell(row=i, column=1).value, worksheet.cell(row=i, column=2).value)
         count += 1
-        if count % 1000 == 0:
+        if count % 5000 == 0:
             print('Report_202 - insert to DB' , count) 
             cnx.commit()
     print('Report_202 - insert to DB' , count) 
     cnx.commit()
     count = 0
     worksheet = wookbook[sheetnames[1]]
-    for i in range(1, worksheet.max_row):
+    for i in range(2, worksheet.max_row):
         add_Info(cnx, 'Total_GL', worksheet.cell(row=i, column=1).value, worksheet.cell(row=i, column=2).value)
         count += 1
-        if count % 1000 == 0:
+        if count % 5000 == 0:
             print('Total_GL - insert to DB' , count) 
             cnx.commit()
     print('Total_GL - insert to DB' , count) 
     cnx.commit()
-    close_Connection(cnx)
 #-----------------------------------------------------------------------------------------------
 def LoadData():
     loadDataFromFile()
