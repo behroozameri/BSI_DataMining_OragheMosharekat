88fbbc1cb438afdf5dcbf3989c88f01f9a2d0a90
Behrooz Ameri <behrooz.ameri@gmail.com>
2023-01-10 14:00:58 +0330

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,6 +1,7 @@
 from SQL_Preprocessing import Preprocessing
 from SQL_Process import Process
 from LoadDataFromFileToDataBase import LoadData
+from Save_Result import Save
 import os
 #-----------------------------------------------------------------------------------------------
 def clear():
@@ -9,6 +10,11 @@ def clear():
 clear()
 print('--- Start Program ---')
 Preprocessing()
-Process()
 LoadData()
+print('-- Start Process Data --')
+Process()
+print('-- End Process Data --')
+print('-- Start Save Data --')
+Save()
+print('-- End Save Data --')
 print('--- End of Program ---')
